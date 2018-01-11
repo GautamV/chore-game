db4f27e402d5f2ea0c1a8973266a99e756978bc7
GautamV <gautamplus@gmail.com>
2018-01-11 18:22:05 -0500

    changed database for heroku

--- db_setup.py
+++ db_setup.py
@@ -5,13 +5,15 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 if __name__ == "__main__":
 
-	# change this for heroku 
+	urlparse.uses_netloc.append("postgres")
+	url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
 	conn = psycopg2.connect(
-		database='chore_gamev4',
-		user='postgres',
-		password='pass',
-		host='localhost',
-		port='5432'
+		database=url.path[1:],
+		user=url.username,
+		password=url.password,
+		host=url.hostname,
+		port=url.port
 	)
 	conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) 
 	cur = conn.cursor()
--- db_utils.py
+++ db_utils.py
@@ -4,13 +4,15 @@ import datetime
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-# change this for heroku 
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
 conn = psycopg2.connect(
-	database='chore_gamev4',
-	user='postgres',
-	password='pass',
-	host='localhost',
-	port='5432'
+	database=url.path[1:],
+	user=url.username,
+	password=url.password,
+	host=url.hostname,
+	port=url.port
 )
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cur = conn.cursor()
